f3fce2a3333198419e69ca6df35759e3965b72e1
RedFantom <redfantom@outlook.com>
2018-05-03 21:19:46 +0200

    Fix issue with update match map, again

--- database/database.py
+++ database/database.py
@@ -121,7 +121,7 @@ class DatabaseHandler(object):
         commands = list()
         kwargs = {
             "match": match,
-            "map": map.replace("'", "\\'"),
+            "map": map.replace("'", "\\'") if map is not None else None,
             "score": score,
             "end": end
         }
