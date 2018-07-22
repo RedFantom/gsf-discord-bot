6339dee49aa6ea664c992027eeac72217ae3d6ad
RedFantom <redfantom@outlook.com>
2018-07-22 18:22:16 +0200

    Fix bot closing after a while

--- main.py
+++ main.py
@@ -5,10 +5,29 @@ Copyright (C) 2018 RedFantom
 """
 # Standard Library
 import asyncio
+from traceback import format_exc
 # Project Modules
 from database import DatabaseHandler
 from bot import DiscordBot
 from server import DiscordServer
+from utils.utils import setup_logger
+
+
+logger = setup_logger("main", "main.log")
+
+
+async def run_bot(bot: DiscordBot, token: str):
+    """Indefinitely run a DiscordBot"""
+    await bot.bot.start(token)
+    while bot.bot.is_logged_in:
+        if bot.bot.is_closed:
+            bot.bot._closed.clear()
+            bot.bot.http.recreate()
+        try:
+            await bot.bot.connect()
+        except Exception:
+            logger.error(format_exc())
+            await asyncio.sleep(60)
 
 
 if __name__ == '__main__':
@@ -19,7 +38,7 @@ if __name__ == '__main__':
     server = DiscordServer(database, loop, "discord.gsfparser.tk", 64731)
     bot = DiscordBot(database, server, loop)
     loop.create_task(server.start())
-    loop.create_task(bot.bot.start(token))
+    loop.create_task(run_bot(bot, token))
     try:
         loop.run_forever()
     except KeyboardInterrupt:
