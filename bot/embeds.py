fa7ed16774d11d4346ea8bc17ce53a5f3f086485
RedFantom <redfantom@outlook.com>
2018-07-25 12:51:43 +0200

    Remove images from crew embeds
    They disturb the clean look of the embeds

--- bot/embeds.py
+++ bot/embeds.py
@@ -5,11 +5,6 @@ Copyright (C) 2018 RedFantom
 """
 # Packages
 from discord import Embed
-# Project Modules
-from utils.utils import setup_logger
-
-
-logger = setup_logger("Embeds", "embeds.log")
 
 
 def embed_from_crew_dict(member: dict) -> Embed:
@@ -21,11 +16,8 @@ def embed_from_crew_dict(member: dict) -> Embed:
     fields = list()
     for key in ["Ability", "Passive", "SecondaryPassive"]:
         fields.append((member["{}Name".format(key)], member["{}Description".format(key)]))
-    image = "http://discord.gsfparser.tk/icons/{}.jpg".format(member["Icon"].lower())
-    logger.debug("Crew Icon: {}".format(image))
     colour = 0xb90505 if member["Faction"] == "Imperial" else 0x085195
     embed = Embed(title=title, description=description, colour=colour)
-    embed.set_image(url=image)
     for header, (name, value) in zip(("Ability", "Passive", "Passive"), fields):
         embed.add_field(name="{}: {}".format(header, name), value=value, inline=False)
     return embed
