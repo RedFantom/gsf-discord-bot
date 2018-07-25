4906f4d6890c91e8ca158d1136fb9701ecab2832
RedFantom <redfantom@outlook.com>
2018-07-25 12:41:45 +0200

    Add logging to embeds module

--- bot/embeds.py
+++ bot/embeds.py
@@ -5,6 +5,11 @@ Copyright (C) 2018 RedFantom
 """
 # Packages
 from discord import Embed
+# Project Modules
+from utils.utils import setup_logger
+
+
+logger = setup_logger("Embeds", "embeds.log")
 
 
 def embed_from_crew_dict(member: dict) -> Embed:
@@ -17,6 +22,7 @@ def embed_from_crew_dict(member: dict) -> Embed:
     for key in ["Ability", "Passive", "SecondaryPassive"]:
         fields.append((member["{}Name".format(key)], member["{}Description".format(key)]))
     image = "http://discord.gsfparser.tk/icons/{}.jpg".format(member["Icon"].lower())
+    logger.debug("Crew Icon: {}".format(image))
     colour = 0xb90505 if member["Faction"] == "Imperial" else 0x085195
     embed = Embed(title=title, description=description, colour=colour)
     embed.set_image(url=image)
