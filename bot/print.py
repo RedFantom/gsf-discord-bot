5953549cf4edf51471267fb2e5c2d84f65d5a6ab
RedFantom <redfantom@outlook.com>
2018-07-25 11:56:44 +0200

    Rename functions in print.py

--- bot/print.py
+++ bot/print.py
@@ -12,7 +12,7 @@ from .messages import *
 from .func import get_download_link, get_server_status
 
 
-async def print_manual(self, channel: Channel, author: DiscordUser, args: tuple):
+async def manual(self, channel: Channel, author: DiscordUser, args: tuple):
     """Send the DiscordBot manual to a channel"""
     if len(args) == 0:
         args = ("commands",)
@@ -26,12 +26,12 @@ async def print_manual(self, channel: Channel, author: DiscordUser, args: tuple)
     await self.bot.send_message(channel, message)
 
 
-async def print_help(self, channel: Channel, user: DiscordUser, args: tuple):
+async def help(self, channel: Channel, user: DiscordUser, args: tuple):
     """Print the HELP message"""
     await self.bot.send_message(channel, HELP)
 
 
-async def print_servers(self, channel: Channel, user: DiscordUser, args: tuple):
+async def servers(self, channel: Channel, user: DiscordUser, args: tuple):
     """Send a list of servers to the channel with server statuses"""
     servers = await get_server_status()
     if servers is None:
@@ -41,25 +41,25 @@ async def print_servers(self, channel: Channel, user: DiscordUser, args: tuple):
     await self.bot.send_message(channel, SERVER_STATUS.format(**servers))
 
 
-async def print_author(self, channel: Channel, user: DiscordUser, args: tuple):
+async def author(self, channel: Channel, user: DiscordUser, args: tuple):
     """Print the author information message as an embed"""
     embed = await self.build_embed(*AUTHOR_EMBED, fields=[CONTENT_LICENSE, CODE_LICENSE], colour=0x0000FF)
     await self.bot.send_message(channel, embed=embed)
 
 
-async def print_privacy(self, channel: Channel, user: DiscordUser, args: tuple):
+async def privacy(self, channel: Channel, user: DiscordUser, args: tuple):
     await self.bot.send_message(channel, PRIVACY)
 
 
-async def print_purpose(self, channel: Channel, user: DiscordUser, args: tuple):
+async def purpose(self, channel: Channel, user: DiscordUser, args: tuple):
     await self.bot.send_message(channel, PURPOSE)
 
 
-async def print_setup(self, channel: Channel, user: DiscordUser, args: tuple):
+async def setup(self, channel: Channel, user: DiscordUser, args: tuple):
     await self.bot.send_message(channel, SETUP)
 
 
-async def print_link(self, channel: Channel, user: DiscordUser, args: tuple):
+async def link(self, channel: Channel, user: DiscordUser, args: tuple):
     links = await get_download_link()
     if links is None:
         await self.bot.send_message(channel, GITHUB_RATE_LIMIT)
